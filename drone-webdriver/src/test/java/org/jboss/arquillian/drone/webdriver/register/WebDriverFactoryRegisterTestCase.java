3752bd882f7a01847815e9aa78a1e956a5d286ff
Matous Jobanek <mjobanek@redhat.com>
2015-10-22 09:36:38 +0200

    ARQ-1692 added test

---
+++ drone-webdriver/src/test/java/org/jboss/arquillian/drone/webdriver/register/WebDriverFactoryRegisterTestCase.java
@@ -0,0 +1,25 @@
+package org.jboss.arquillian.drone.webdriver.register;
+
+import org.jboss.arquillian.drone.api.annotation.Drone;
+import org.jboss.arquillian.junit.Arquillian;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.WebDriver;
+
+import static org.junit.Assert.assertNotNull;
+
+
+/**
+ * @author <a href="mailto:mjobanek@redhat.com">Matous Jobanek</a>
+ */
+@RunWith(Arquillian.class)
+public class WebDriverFactoryRegisterTestCase {
+
+    @Drone
+    private WebDriver browser;
+
+    @Test
+    public void verifyDrone() {
+        assertNotNull(browser);
+    }
+}
