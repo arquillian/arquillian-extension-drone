137342c8424ec07172e2decbd18e7a98a41c86bf
Matous Jobanek <mjobanek@redhat.com>
2016-03-10 16:39:01 +0100

    ARQ-2025 Arquillian Drone BrowserStack extension

---
+++ extension/arquillian-drone-browserstack-extension/src/main/java/org/arquillian/drone/browserstack/extension/webdriver/BrowserStackCapabilities.java
@@ -0,0 +1,58 @@
+/**
+ * JBoss, Home of Professional Open Source
+ * Copyright 2016, Red Hat Middleware LLC, and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.arquillian.drone.browserstack.extension.webdriver;
+
+import java.util.Map;
+
+import org.jboss.arquillian.drone.webdriver.spi.BrowserCapabilities;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+/**
+ * An internal mapping between browser capabilities property, implementation class and DesiredCapabilities. This class also
+ * supports implementationClass property which is now legacy configuration value.
+ *
+ * @author <a href="mailto:mjobanek@redhat.com">Matous Jobanek</a>
+ * @see DesiredCapabilities
+ */
+public class BrowserStackCapabilities implements BrowserCapabilities {
+
+    public static final String READABLE_NAME = "browserstack";
+    public static final String USERNAME = "username";
+    public static final String ACCESS_KEY = "access.key";
+    public static final String URL = "url";
+    public static final String BROWSERSTACK_LOCAL_MANAGED = "browserstack.local.managed";
+    public static final String BROWSERSTACK_LOCAL_BINARY = "browserstack.local.binary";
+    public static final String BROWSERSTACK_LOCAL_ARGS = "browserstack.local.args";
+
+    public static final String BROWSERSTACK_LOCAL = "browserstack.local";
+
+    public String getImplementationClassName() {
+        return BrowserStackDriver.class.getName();
+    }
+
+    public Map<String, ?> getRawCapabilities() {
+        return new DesiredCapabilities().asMap();
+    }
+
+    public String getReadableName() {
+        return READABLE_NAME;
+    }
+
+    public int getPrecedence() {
+        return 0;
+    }
+}
---
+++ extension/arquillian-drone-browserstack-extension/src/test/java/org/arquillian/drone/browserstack/extension/webdriver/WebDriverUtil.java
@@ -0,0 +1,98 @@
+/**
+ * JBoss, Home of Professional Open Source
+ * Copyright 2016, Red Hat Middleware LLC, and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.arquillian.drone.browserstack.extension.webdriver;
+
+import com.google.common.base.Function;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.StaleElementReferenceException;
+import org.openqa.selenium.TimeoutException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+/**
+ * Util class to make UI tests easier
+ *
+ * @author <a href="mailto:mjobanek@redhat.com">Matous Jobanek</a>
+ */
+public class WebDriverUtil {
+
+    protected static void checkElementIsPresent(final WebDriver driver, final By by, final String errorMsg) {
+        new WebDriverWaitWithMessage(driver, 10).failWith(errorMsg).until(new ExpectedCondition<Boolean>() {
+            @Override public Boolean apply(WebDriver webDriver) {
+                try {
+                    return driver.findElement(by) != null;
+                } catch (NoSuchElementException ignored) {
+                    return false;
+                } catch (StaleElementReferenceException ignored) {
+                    return false;
+                }
+            }
+        });
+    }
+
+    protected static void checkElementContent(final WebDriver driver, final By by, final String expectedContent,
+        final String errorMsg) {
+        new WebDriverWaitWithMessage(driver, 10).failWith(errorMsg).until(new ExpectedCondition<Boolean>() {
+            @Override
+            public Boolean apply(WebDriver webDriver) {
+                try {
+                    String text = driver.findElement(by).getText();
+                    if (text != null) {
+                        return text.contains(expectedContent);
+                    }
+                    return false;
+                } catch (NoSuchElementException ignored) {
+                    return false;
+                } catch (StaleElementReferenceException ignored) {
+                    return false;
+                }
+            }
+        });
+    }
+
+    protected static class WebDriverWaitWithMessage extends WebDriverWait {
+
+        private String message;
+
+        public WebDriverWaitWithMessage(WebDriver driver, long timeOutInSeconds) {
+            super(driver, timeOutInSeconds);
+        }
+
+        public WebDriverWait failWith(String message) {
+            if (message == null || message.length() == 0) {
+                throw new IllegalArgumentException("Error message must not be null nor empty");
+            }
+            this.message = message;
+            return this;
+        }
+
+        @Override
+        public <V> V until(Function<? super WebDriver, V> isTrue) {
+            if (message == null) {
+                return super.until(isTrue);
+            } else {
+                try {
+                    return super.until(isTrue);
+                } catch (TimeoutException e) {
+                    throw new TimeoutException(message, e);
+                }
+            }
+        }
+    }
+}
