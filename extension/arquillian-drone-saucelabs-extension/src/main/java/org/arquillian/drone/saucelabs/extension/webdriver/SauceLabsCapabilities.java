595ce3c10461a06fff63a29de68b66fa2e551e76
Matous Jobanek <mjobanek@redhat.com>
2016-03-15 07:39:53 +0100

    ARQ-1694 Arquillian Drone Sauce Labs extension

---
+++ extension/arquillian-drone-saucelabs-extension/src/main/java/org/arquillian/drone/saucelabs/extension/webdriver/SauceLabsCapabilities.java
@@ -0,0 +1,55 @@
+/**
+ * JBoss, Home of Professional Open Source
+ * Copyright 2016, Red Hat Middleware LLC, and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.arquillian.drone.saucelabs.extension.webdriver;
+
+import java.util.Map;
+
+import org.jboss.arquillian.drone.webdriver.spi.BrowserCapabilities;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+/**
+ * An internal mapping between browser capabilities property, implementation class and DesiredCapabilities. This class also
+ * supports implementationClass property which is now legacy configuration value.
+ *
+ * @author <a href="mailto:mjobanek@redhat.com">Matous Jobanek</a>
+ */
+public class SauceLabsCapabilities implements BrowserCapabilities {
+
+    public static final String READABLE_NAME = "saucelabs";
+    public static final String USERNAME = "username";
+    public static final String ACCESS_KEY = "access.key";
+    public static final String URL = "url";
+    public static final String SAUCE_CONNECT_MANAGED = "sauce.connect.managed";
+    public static final String SAUCE_CONNECT_ARGS = "sauce.connect.args";
+    public static final String SAUCE_CONNECT_BINARY = "sauce.connect.binary";
+
+    public String getImplementationClassName() {
+        return SauceLabsDriver.class.getName();
+    }
+
+    public Map<String, ?> getRawCapabilities() {
+        return new DesiredCapabilities().asMap();
+    }
+
+    public String getReadableName() {
+        return READABLE_NAME;
+    }
+
+    public int getPrecedence() {
+        return 0;
+    }
+}
---
+++ extension/arquillian-drone-saucelabs-extension/src/main/java/org/arquillian/drone/saucelabs/extension/webdriver/SauceLabsDriver.java
@@ -0,0 +1,62 @@
+/**
+ * JBoss, Home of Professional Open Source
+ * Copyright 2016, Red Hat Middleware LLC, and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.arquillian.drone.saucelabs.extension.webdriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.logging.Logger;
+
+import org.openqa.selenium.Capabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+/**
+ * This is an implementation of the {@link RemoteWebDriver} intended to be used with Sauce Labs account.
+ * saucelabs.com is a cloud-based cross-browser grid for executing Selenium WebDriver tests. See saucelabs.com
+ *
+ * @author <a href="mailto:mjobanek@redhat.com">Matous Jobanek</a>
+ */
+public class SauceLabsDriver extends RemoteWebDriver {
+
+    private static final Logger log = Logger.getLogger(SauceLabsDriver.class.getName());
+
+    private final boolean isSetSauceConnectManaged;
+
+    public SauceLabsDriver(URL url, Capabilities capabilities, boolean isSetSauceConnectManaged) {
+        super(url, capabilities);
+        this.isSetSauceConnectManaged = isSetSauceConnectManaged;
+    }
+
+    @Override
+    public void get(String url) {
+        String host = null;
+        try {
+            host = new URL(url).getHost();
+        } catch (MalformedURLException e) {
+            log.warning("The url " + url + " has been detected as a malformed URL. The message of the exception: " + e
+                .getMessage());
+        }
+
+        if (host != null && (host.equals("localhost") || host.equals("127.0.0.1"))) {
+            if (!isSetSauceConnectManaged) {
+                log.info(
+                    "To test against localhost and other locations behind your firewall, you need to use Sauce Connect. "
+                        + "You can ignore this if you have already started it, otherwise see saucelabs.com/connect");
+            }
+        }
+        super.get(url);
+    }
+}
