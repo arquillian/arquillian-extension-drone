4b6e1d587238fd877934491da9821510d57c2263
Matous Jobanek <mjobanek@redhat.com>
2016-03-16 12:43:19 +0100

    java6 compatibility, licences, refactoring

--- extension/arquillian-drone-browserstack-extension/src/main/java/org/arquillian/drone/browserstack/extension/local/BrowserStackLocalRunner.java
+++ extension/arquillian-drone-browserstack-extension/src/main/java/org/arquillian/drone/browserstack/extension/local/BrowserStackLocalRunner.java
@@ -1,3 +1,19 @@
+/**
+ * JBoss, Home of Professional Open Source
+ * Copyright 2016, Red Hat Middleware LLC, and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.arquillian.drone.browserstack.extension.local;
 
 import java.io.BufferedReader;
@@ -8,7 +24,10 @@ import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.Callable;
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.Executors;
+import java.util.concurrent.FutureTask;
 import java.util.concurrent.TimeUnit;
 import java.util.logging.Logger;
 
@@ -176,11 +195,13 @@ public class BrowserStackLocalRunner {
             BufferedReader in = new BufferedReader(new InputStreamReader(process.getInputStream()));
             String line;
             boolean isAlreadyRunning = false;
+            FutureTask<Boolean> futureTask = new FutureTask<Boolean>(new WaitForProcess());
+            Executors.newSingleThreadExecutor().submit(futureTask);
 
             while (!isAlreadyRunning) {
                 try {
                     synchronized (process) {
-                        if (!process.isAlive()) {
+                        if (futureTask.isDone()) {
                             break;
                         }
                         while (in.ready() && (line = in.readLine()) != null) {
@@ -206,6 +227,17 @@ public class BrowserStackLocalRunner {
                     e.printStackTrace();
                 }
             }
+        }
+    }
+
+    /**
+     * Waits until the SauceConnect binary process ends
+     */
+    private class WaitForProcess implements Callable<Boolean> {
+        @Override
+        public Boolean call() throws Exception {
+            process.waitFor();
+            return true;
         }
     }
 
