c79e5b1f37c9b7aeee6e51840f1e44c69d60e2ce
hemanik <hemanikatyal@gmail.com>
2017-02-07 16:53:24 +0530

    Updates test cases to reflect actual behaviour of command.

--- drone-webdriver/src/test/java/org/jboss/arquillian/drone/webdriver/binary/process/SeleniumServerTestCase.java
+++ drone-webdriver/src/test/java/org/jboss/arquillian/drone/webdriver/binary/process/SeleniumServerTestCase.java
@@ -1,7 +1,7 @@
 package org.jboss.arquillian.drone.webdriver.binary.process;
 
-import org.assertj.core.api.Assertions;
 import org.jboss.arquillian.drone.webdriver.binary.handler.SeleniumServerBinaryHandler;
+import org.jboss.arquillian.junit.InSequence;
 import org.jboss.arquillian.test.spi.event.suite.AfterSuite;
 import org.jboss.arquillian.test.test.AbstractTestTestBase;
 import org.junit.After;
@@ -12,6 +12,7 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.io.PrintStream;
 import java.net.URL;
 import java.util.List;
 import java.util.logging.Handler;
@@ -20,11 +21,16 @@ import java.util.logging.StreamHandler;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class SeleniumServerTestCase extends AbstractTestTestBase {
 
     private static Logger log = Logger.getLogger(SeleniumServerExecutor.class.toString());
     private OutputStream logCapturingStream;
     private StreamHandler customLogHandler;
+
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
 
     private String seleniumServerBinary;
     private DesiredCapabilities capabilities;
@@ -41,53 +47,88 @@ public class SeleniumServerTestCase extends AbstractTestTestBase {
                 new SeleniumServerBinaryHandler(new DesiredCapabilities()).downloadAndPrepare().toString();
         capabilities = new DesiredCapabilities();
         url = new URL("http://localhost:4444/wd/hub/");
+
         attachLogCapture();
+        setUpStreams();
     }
 
     @Test
-    public void test_command_contains_serverArgs() throws Exception {
+    @InSequence(1)
+    public void should_start_selenium_server_with_serverArgs_debug() throws Exception {
 
         final String browser = "chrome";
-        final String seleniumServerArgs = "-debug";
+        final String seleniumServerArgs = "-debug -role node";
 
         fire(new StartSeleniumServer(seleniumServerBinary, browser, capabilities, url, seleniumServerArgs));
 
-        final String capturedLog = getTestCapturedLog();
-        final String output = parseLogger(capturedLog);
+        final String command = parseLogger();
 
-        Assertions.assertThat(output.contains(seleniumServerArgs));
+        assertThat(command).contains(seleniumServerArgs);
+        assertThat(outContent.toString()).contains("DEBUG");
     }
 
     @Test
-    public void test_command_does_not_contain_serverArgs() throws Exception {
+    @InSequence(2)
+    public void should_start_selenium_server_with_no_serverArgs() throws Exception {
 
         final String browser = "chrome";
 
         fire(new StartSeleniumServer(seleniumServerBinary, browser, capabilities, url, null));
 
-        final String capturedLog = getTestCapturedLog();
-        final String output = parseLogger(capturedLog);
+        final String command = parseLogger();
 
-        Assertions.assertThat(output.endsWith(String.valueOf(url.getPort())));
+        assertThat(command).endsWith(String.valueOf(url.getPort()));
+        assertThat(outContent.toString()).contains("Selenium Server is up and running");
+    }
+
+    @Test
+    @InSequence(3)
+    public void should_start_selenium_server_with_hub_and_node() throws Exception {
+
+        final String browser = "chrome";
+
+        // start selenium Grid Hub at port 4444
+        String seleniumServerArgs = "-role hub -browserTimeout 1000";
+
+        fire(new StartSeleniumServer(seleniumServerBinary, browser, capabilities, url, seleniumServerArgs));
+
+        String hubCommand = parseLogger();
+
+        assertThat(hubCommand).contains(seleniumServerArgs);
+        assertThat(outContent.toString()).contains("Selenium Grid hub is up and running");
+
+        // start selenium grid node at port 5555 and register it with the Hub.
+        String seleniumServerArgs2 = "-role node";
+        URL nodeUrl = new URL("http://localhost:5555");
+
+        fire(new StartSeleniumServer(seleniumServerBinary, browser, capabilities, nodeUrl, seleniumServerArgs2));
+
+        String nodeCommand = parseLogger();
+
+        assertThat(nodeCommand).contains(seleniumServerArgs2);
+        assertThat(outContent.toString()).contains("The node is registered to the hub and ready to use");
     }
 
     @After
-    public void stopSeleniumServer(){
+    public void stopSeleniumServer() {
+        cleanUpStreams();
         fire(new AfterSuite());
     }
 
-    private String parseLogger(String capturedLog) {
+    private String parseLogger() throws IOException {
 
+        final String capturedLog = getTestCapturedLog();
         final String expectedLogPart = "Running Selenium server process: java .*$";
+
         final Pattern pattern = Pattern.compile(expectedLogPart);
         final Matcher matcher = pattern.matcher(capturedLog);
-        String line;
+        String command;
 
         if (matcher.find()) ;
         {
-            line = matcher.group();
+            command = matcher.group();
         }
-        return line;
+        return command;
     }
 
     private void attachLogCapture() {
@@ -100,5 +141,15 @@ public class SeleniumServerTestCase extends AbstractTestTestBase {
     private String getTestCapturedLog() throws IOException {
         customLogHandler.flush();
         return logCapturingStream.toString();
+    }
+
+    private void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+        System.setErr(new PrintStream(errContent));
+    }
+
+    private void cleanUpStreams() {
+        System.setOut(null);
+        System.setErr(null);
     }
 }
