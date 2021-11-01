9b04b1055847844d7dde9eb7994b981794b801a9
Andy Fedotov <andy.fedotov@gmail.com>
2021-11-01 17:39:34 +0300

    fix: supports geckodriver binary for aarch64(#261)
    Co-authored-by: Bartosz Majsak <bartosz.majsak@gmail.com>
    
    Along with fixing wrong file pickup on x86_64 
    Fixes #259

--- drone-webdriver/src/main/java/org/jboss/arquillian/drone/webdriver/binary/downloading/source/GeckoDriverGitHubSource.java
+++ drone-webdriver/src/main/java/org/jboss/arquillian/drone/webdriver/binary/downloading/source/GeckoDriverGitHubSource.java
@@ -7,33 +7,42 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class GeckoDriverGitHubSource extends GitHubSource {
 
-    public GeckoDriverGitHubSource(HttpClient httpClient, GitHubLastUpdateCache gitHubLastUpdateCache,
-        DesiredCapabilities capabilities) {
+    public GeckoDriverGitHubSource(HttpClient httpClient,
+                                   GitHubLastUpdateCache gitHubLastUpdateCache,
+                                   DesiredCapabilities capabilities) {
         super("mozilla", "geckodriver", httpClient, gitHubLastUpdateCache, capabilities);
     }
 
     @Override
     public String getFileNameRegexToDownload(String version) {
-        StringBuilder fileNameRegex = new StringBuilder("geckodriver-");
+        StringBuilder fileNameRegex = new StringBuilder("^geckodriver-");
         fileNameRegex.append(version).append("-");
         if (PlatformUtils.isMac()) {
-            fileNameRegex.append("macos").toString();
-        } else {
-            if (PlatformUtils.isWindows() || PlatformUtils.isUnix()) {
-                if (PlatformUtils.isWindows()) {
-                    fileNameRegex.append("win");
-                } else {
-                    fileNameRegex.append("linux");
-                }
-                if (PlatformUtils.is32()) {
-                    fileNameRegex.append("32").toString();
-                } else {
-                    fileNameRegex.append("64").toString();
-                }
-            } else {
-                fileNameRegex.append("arm7hf").toString();
+            fileNameRegex.append("macos");
+            if (PlatformUtils.isMacAppleSilicon()) {
+                fileNameRegex.append("-aarch64");
             }
+        } else if (PlatformUtils.isWindows() || PlatformUtils.isLinux()) {
+            if (PlatformUtils.isWindows()) {
+                fileNameRegex.append("win");
+            } else {
+                fileNameRegex.append("linux");
+            }
+            if (PlatformUtils.is32()) {
+                fileNameRegex.append("32");
+            } else {
+                fileNameRegex.append("64");
+            }
+        } else {
+            throw new RuntimeException(
+                String.format(
+                    "GeckoDriver binary download is not available for os.name = %s, os.arch = %s",
+                    System.getProperty("os.name"),
+                    System.getProperty("os.arch")
+                )
+            );
         }
-        return fileNameRegex.append(".*").toString();
+        return fileNameRegex.append("\\.(zip|tgz|tar\\.gz)$").toString();
     }
+
 }
