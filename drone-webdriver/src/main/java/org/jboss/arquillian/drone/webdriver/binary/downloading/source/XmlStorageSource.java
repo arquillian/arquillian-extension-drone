97da4a4d07e75b81428415630240b25f96c16cf5
Bartosz Majsak <bartosz.majsak@gmail.com>
2021-11-25 16:36:11 +0100

    chore: applies suggestions from code review

--- drone-webdriver/src/main/java/org/jboss/arquillian/drone/webdriver/binary/downloading/source/XmlStorageSource.java
+++ drone-webdriver/src/main/java/org/jboss/arquillian/drone/webdriver/binary/downloading/source/XmlStorageSource.java
@@ -82,15 +82,13 @@ public abstract class XmlStorageSource implements ExternalBinarySource {
         return getReleaseForVersion(latestVersion);
     }
 
-    protected String getVersion(String urlToLatestRelease, String charset) throws IOException {
-        return StringUtils.trimMultiline(httpClient.get(urlToLatestRelease, charset).getPayload());
+    protected String getVersion(String versionUrl, String charset) throws IOException {
+        return StringUtils.trimMultiline(httpClient.get(versionUrl, charset).getPayload());
     }
 
     private List<DriverEntry> retrieveAllDriversEntries() throws Exception {
         final List<DriverEntry> results = new ArrayList<>();
-        final DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-        dbf.setIgnoringElementContentWhitespace(true);
-        final DocumentBuilder db = dbf.newDocumentBuilder();
+        final DocumentBuilder db = DocumentBuilderFactory.newInstance().newDocumentBuilder();
         final InputSource is = new InputSource();
         is.setByteStream(new BOMInputStream(new ByteArrayInputStream(httpClient.get(storageUrl).getPayload().getBytes(StandardCharsets.UTF_8))));
         final Document doc = db.parse(is);
