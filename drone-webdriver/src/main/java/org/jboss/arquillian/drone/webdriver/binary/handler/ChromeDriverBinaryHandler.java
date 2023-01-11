37d913d30b1e97d449a5818354785741b47cfba3
Michael Stringer <stringbean@users.noreply.github.com>
2023-01-11 12:20:35 +0000

    Fix ChromeDriver extraction if archive has license

--- drone-webdriver/src/main/java/org/jboss/arquillian/drone/webdriver/binary/handler/ChromeDriverBinaryHandler.java
+++ drone-webdriver/src/main/java/org/jboss/arquillian/drone/webdriver/binary/handler/ChromeDriverBinaryHandler.java
@@ -1,9 +1,11 @@
 package org.jboss.arquillian.drone.webdriver.binary.handler;
 
+import java.io.File;
 import java.util.Objects;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import org.jboss.arquillian.drone.webdriver.binary.BinaryFilesUtils;
 import org.jboss.arquillian.drone.webdriver.binary.downloading.ExternalBinary;
 import org.jboss.arquillian.drone.webdriver.binary.downloading.source.ExternalBinarySource;
 import org.jboss.arquillian.drone.webdriver.binary.downloading.source.MissingBinaryException;
@@ -25,6 +27,8 @@ public class ChromeDriverBinaryHandler extends AbstractBinaryHandler {
     public static final String CHROME_DRIVER_BINARY_PROPERTY = "chromeDriverBinary";
     private static final String CHROME_DRIVER_VERSION_PROPERTY = "chromeDriverVersion";
     private static final String CHROME_DRIVER_URL_PROPERTY = "chromeDriverUrl";
+
+    public static final String CHROME_DRIVER_BINARY_NAME = "chromedriver" + (PlatformUtils.isWindows() ? ".exe" : "");
 
     private final DesiredCapabilities capabilities;
 
@@ -65,6 +69,19 @@ public class ChromeDriverBinaryHandler extends AbstractBinaryHandler {
     @Override
     public String getSystemBinaryProperty() {
         return CHROME_SYSTEM_DRIVER_BINARY_PROPERTY;
+    }
+
+    @Override
+    protected File prepare(File downloaded) throws Exception {
+        File extraction = BinaryFilesUtils.extract(downloaded);
+        File[] files = extraction.listFiles(file -> file.isFile() && file.getName().equals(CHROME_DRIVER_BINARY_NAME));
+
+        if (files == null || files.length != 1) {
+            throw new IllegalStateException(
+                "Missing ChromeDriver executable (" + CHROME_DRIVER_BINARY_NAME + " in the directory " + extraction);
+        }
+
+        return markAsExecutable(files[0]);
     }
 
     public static class ChromeStorageSources extends UrlStorageSource {
