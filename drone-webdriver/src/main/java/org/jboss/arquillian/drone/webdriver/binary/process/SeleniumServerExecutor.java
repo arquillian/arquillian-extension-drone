b8b929b4bf6648a01aa5598dd56fcb4b31b2bdc6
Martin Bartoš <mabartos@redhat.com>
2023-10-11 14:26:53 +0200

    Download Chrome Driver with versions >= 115
    Closes #420
    
    Closes #405

--- drone-webdriver/src/main/java/org/jboss/arquillian/drone/webdriver/binary/process/SeleniumServerExecutor.java
+++ drone-webdriver/src/main/java/org/jboss/arquillian/drone/webdriver/binary/process/SeleniumServerExecutor.java
@@ -6,6 +6,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
+import java.util.function.Supplier;
 import java.util.logging.Logger;
 import org.arquillian.spacelift.Spacelift;
 import org.arquillian.spacelift.execution.Execution;
@@ -19,11 +20,13 @@ import org.jboss.arquillian.core.api.annotation.Inject;
 import org.jboss.arquillian.core.api.annotation.Observes;
 import org.jboss.arquillian.drone.webdriver.binary.handler.BinaryHandler;
 import org.jboss.arquillian.drone.webdriver.binary.handler.ChromeDriverBinaryHandler;
+import org.jboss.arquillian.drone.webdriver.binary.handler.ChromeForTestingDriverBinaryHandler;
 import org.jboss.arquillian.drone.webdriver.binary.handler.EdgeDriverBinaryHandler;
 import org.jboss.arquillian.drone.webdriver.binary.handler.FirefoxDriverBinaryHandler;
 import org.jboss.arquillian.drone.webdriver.binary.handler.InternetExplorerBinaryHandler;
 import org.jboss.arquillian.drone.webdriver.binary.handler.PhantomJSDriverBinaryHandler;
 import org.jboss.arquillian.drone.webdriver.factory.BrowserCapabilitiesList;
+import org.jboss.arquillian.drone.webdriver.utils.ChromeUtils;
 import org.jboss.arquillian.drone.webdriver.utils.Validate;
 import org.jboss.arquillian.test.spi.event.suite.AfterSuite;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -104,12 +107,21 @@ public class SeleniumServerExecutor {
      * @return An instance of a {@link BinaryHandler} according to given browser
      */
     private BinaryHandler getBrowserBinaryHandler(DesiredCapabilities capabilities, String browser) {
+        final Supplier<BinaryHandler> chromeBinaryHandler = () -> {
+            final String version = ChromeUtils.getChromeVersion(capabilities);
+            if (ChromeUtils.isChromeForTesting(version)) {
+                return new ChromeForTestingDriverBinaryHandler(capabilities);
+            } else {
+                return new ChromeDriverBinaryHandler(capabilities);
+            }
+        };
+
         if (new BrowserCapabilitiesList.Firefox().getReadableName().equals(browser)) {
             return new FirefoxDriverBinaryHandler(capabilities);
         } else if (new BrowserCapabilitiesList.Edge().getReadableName().equals(browser)) {
             return new EdgeDriverBinaryHandler(capabilities);
         } else if (new BrowserCapabilitiesList.Chrome().getReadableName().equals(browser)) {
-            return new ChromeDriverBinaryHandler(capabilities);
+            return chromeBinaryHandler.get();
         } else if (new BrowserCapabilitiesList.InternetExplorer().getReadableName().equals(browser)) {
             return new InternetExplorerBinaryHandler(capabilities);
         } else if (new BrowserCapabilitiesList.PhantomJS().getReadableName().equals(browser)) {
@@ -120,7 +132,7 @@ public class SeleniumServerExecutor {
                 + " You can also execute Selenium server on command line by adding PhantomJS driver jar on classpath.");
             return new PhantomJSDriverBinaryHandler(capabilities);
         } else if (new BrowserCapabilitiesList.ChromeHeadless().getReadableName().equals(browser)) {
-            return new ChromeDriverBinaryHandler(capabilities);
+            return chromeBinaryHandler.get();
         }
         return null;
     }
