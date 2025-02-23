6910dcf06e9ce6b4a7f484511f932c22b58c7a08
lprimak <lenny@flowlogix.com>
2025-02-23 00:58:21 -0600

    fix failing test

--- drone-webdriver/src/main/java/org/jboss/arquillian/drone/webdriver/augmentation/AugmentingEnhancer.java
+++ drone-webdriver/src/main/java/org/jboss/arquillian/drone/webdriver/augmentation/AugmentingEnhancer.java
@@ -24,6 +24,7 @@ import org.jboss.arquillian.drone.webdriver.factory.remote.reusable.ReusableRemo
 import org.jboss.arquillian.drone.webdriver.spi.DroneAugmented;
 import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.MutableCapabilities;
+import org.openqa.selenium.devtools.CdpEndpointFinder;
 import org.openqa.selenium.remote.Augmenter;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
@@ -80,7 +81,10 @@ public class AugmentingEnhancer implements DroneInstanceEnhancer<RemoteWebDriver
      */
     @Override
     public RemoteWebDriver enhance(RemoteWebDriver instance, Class<? extends Annotation> qualifier) {
-        return (RemoteWebDriver) augmenter.augment(instance);
+        if (CdpEndpointFinder.getReportedUri(instance.getCapabilities()).isPresent()) {
+            return (RemoteWebDriver) augmenter.augment(instance);
+        }
+        return instance;
     }
 
     /**
